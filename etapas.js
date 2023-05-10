19fe2c2c3fb5511ad95b53db596a737c1ecbdd1a
Gabriel Roux <126813901+RXGabriel@users.noreply.github.com>
2023-05-10 14:57:56 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Fulano de Tal",
+        partido: "ABC",
+        fotos: [{ url: "vereador-1.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "77222",
+        nome: "Beltrano da Silva",
+        partido: "DEFG",
+        fotos: [{ url: "vereador-2.png", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { url: "Candidato99.jpg", legenda: "Prefeito" },
+          { url: "vice-99.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "Candidato-84.jpg", legenda: "Prefeito" },
+          { url: "Vice-84.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
